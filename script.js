8883ea94f8f55148c86ef8602328ee41683bf58c
Mayur Chopra <mayur5c@users.noreply.github.com>
2023-02-19 20:39:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,14 +6,12 @@ function minimumCost() {
     inputArray = inputArray.split(',').map(Number);
     for (let i = 0; i < inputArray.length - 1; i += 0) {
         if (inputArray.length == 0) break;
+		// sort by decreasing order
         inputArray = inputArray.sort((a,b) => b-a);
         res = inputArray.pop() + inputArray.pop();
         cost += res;
-        inputArray[inputArray.length] = res;
-       
-        
-    }
-
-    
+		// adding res to arrayy
+        inputArray[inputArray.length] = res;   
+    }  
     document.getElementById("result").innerHTML = cost;
 }
